562aa0149aa3f5ef709631a0310d806c6fc41cb6
Dmitry_Bel <dmitry.belanohi@gmail.com>
2017-11-07 19:23:13 +0300

    01

--- js/weather.js
+++ js/weather.js
@@ -6,7 +6,7 @@ let getForecastByLatLng = (location) => {
 			precipProbability = document.getElementById('precipProbability'),
 			pressure = document.getElementById('pressure'),
 			humidity = document.getElementById('humidity');
-	fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/d113af5f82393ef553f48314ae9f42e8/53.890838,27.5372046?lang=ru&units=si')
+	fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/d113af5f82393ef553f48314ae9f42e8/${long},${latd}?lang=ru&units=si`)
 		.then(r => r.json())
 		.then(data => {
 			let	temperature = Math.round((data.currently.temperature - 32) * 5 / 9);
