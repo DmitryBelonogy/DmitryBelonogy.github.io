b15630332f73456d55c32be2f7f1f34ee48bfc54
Dmitry_Bel <dmitry.belanohi@gmail.com>
2017-11-07 18:00:21 +0300

    01

--- js/weather.js
+++ js/weather.js
@@ -6,7 +6,7 @@ let getForecastByLatLng = (location) => {
 			precipProbability = document.getElementById('precipProbability'),
 			pressure = document.getElementById('pressure'),
 			humidity = document.getElementById('humidity');
-	fetch(`http://cors-proxy.htmldriven.com/?url=https://api.darksky.net/forecast/d113af5f82393ef553f48314ae9f42e8/${long},${latd}?lang=ru&units=si`)
+	fetch(`http://api.darksky.net/forecast/d113af5f82393ef553f48314ae9f42e8/${long},${latd}?lang=ru&units=si`)
   	.then((req) => req.json())
 		.then((data) => {
 			let obj = JSON.parse(data.body);
