117a5633d43a3df19fbf7c4b4ba2be9367a7a559
Dmitry_Bel <dmitry.belanohi@gmail.com>
2017-11-07 19:20:00 +0300

    01

--- js/weather.js
+++ js/weather.js
@@ -6,9 +6,16 @@ let getForecastByLatLng = (location) => {
 			precipProbability = document.getElementById('precipProbability'),
 			pressure = document.getElementById('pressure'),
 			humidity = document.getElementById('humidity');
-			fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/d113af5f82393ef553f48314ae9f42e8/53.890838,27.5372046?lang=ru&units=si')
-			.then(r => r.json())
-			.then(data => console.log(data));
+	fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/d113af5f82393ef553f48314ae9f42e8/53.890838,27.5372046?lang=ru&units=si')
+		.then(r => r.json())
+		.then(data => {
+			let	temperature = Math.round((data.currently.temperature - 32) * 5 / 9);
+			temp.innerHTML = temperature;
+			summary.innerHTML = data.currently.summary;
+			precipProbability.innerHTML = 'Вероятность осадков ' + data.currently.precipProbability * 100 + '%';
+			pressure.innerHTML = 'Атмосферное давление ' + data.currently.pressure + 'Па';
+			humidity.innerHTML = 'Влажность ' + data.currently.humidity * 100 + '%';
+		});
 /*			
 	fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/d113af5f82393ef553f48314ae9f42e8/53.890838,27.5372046?lang=ru&units=si')
   	.then((req) => {
