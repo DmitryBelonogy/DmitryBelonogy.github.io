a2717242ce0edb598245e7b63763e07dbd3c40c5
Dmitry_Bel <dmitry.belanohi@gmail.com>
2017-11-07 19:21:37 +0300

    01

--- js/weather.js
+++ js/weather.js
@@ -16,22 +16,5 @@ let getForecastByLatLng = (location) => {
 			pressure.innerHTML = 'Атмосферное давление ' + data.currently.pressure + 'Па';
 			humidity.innerHTML = 'Влажность ' + data.currently.humidity * 100 + '%';
 		});
-/*			
-	fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/d113af5f82393ef553f48314ae9f42e8/53.890838,27.5372046?lang=ru&units=si')
-  	.then((req) => {
-  		console.log(req);
-  		req.json()
-  	})
-		.then((data) => {
-			console.log(data);
-//			let obj = JSON.parse(data.body);
-//			let	temperature = Math.round((obj.currently.temperature - 32) * 5 / 9);
-//			temp.innerHTML = temperature;
-//			summary.innerHTML = obj.currently.summary;
-//			precipProbability.innerHTML = 'Вероятность осадков ' + obj.currently.precipProbability * 100 + '%';
-//			pressure.innerHTML = 'Атмосферное давление ' + obj.currently.pressure + 'Па';
-//			humidity.innerHTML = 'Влажность ' + obj.currently.humidity * 100 + '%';			
-		});
-*/		
 };
 
